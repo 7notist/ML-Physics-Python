df58a6ff655eb3ce332c115860faa3554589e136
7notist <seunghunbag89@gmail.com>
2024-03-02 01:28:05 -0600

    Enter

--- ML-Physics-Python/main.py
+++
@@ -1,7 +0,0 @@
-import torch
-import math
-
-# Define the equation to be approximated
-def range_equation(y0, v0, t, theta):
-    # Corrected mathematical expression with proper operators and function calls
-    return y0 + v0 * t * math.sin(theta) - (1/2) * 9.8 * t**2
